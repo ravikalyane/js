b5cab327df579244b05db2b4273e3a98a72c7a9e
ravikalyane <raviikalyane@gmail.com>
2020-05-20 18:10:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+function blue() {
+    document.getElementById("color").innerHTML = "<h1>Blue</h1>";
+    document.getElementById("color").style.backgroundColor = "blue";
+    document.getElementById("image").src = "blue.png";
+    document.getElementById("color").style.color = "white"
+}
+function red() {
+    document.getElementById("color").innerHTML = "<h1>Red</h1>";
+    document.getElementById("color").style.backgroundColor = "red";
+    document.getElementById("image").src = "red.png";
+    document.getElementById("color").style.color = "white"
+}
+function green() {
+    document.getElementById("color").innerHTML = "<h1>Green</h1>";
+    document.getElementById("color").style.backgroundColor = "green";
+    document.getElementById("image").src = "green.png";
+
+}
+function yellow() {
+    document.getElementById("color").innerHTML = "<h1>Yellow</h1>";
+    document.getElementById("color").style.backgroundColor = "yellow";
+    document.getElementById("image").src = "yellow.png";
+    document.getElementById("color").style.color = "black";
+}
+function white() {
+    document.getElementById("color").innerHTML = "<h1>White</h1>";
+    document.getElementById("color").style.backgroundColor = "white";
+    document.getElementById("image").src = "white.png";
+}
+function lightblue() {
+    document.getElementById("color").innerHTML = "<h1>Light Blue</h1>";
+    document.getElementById("color").style.backgroundColor = "lightblue";
+    document.getElementById("image").src = "lightblue.png";
+    document.getElementById("color").style.color = "black"
+}
+function orange() {
+    document.getElementById("color").innerHTML = "<h1>Orange</h1>";
+    document.getElementById("color").style.backgroundColor = "orange";
+    document.getElementById("image").src = "orange.png";
+    document.getElementById("color").style.color = "white"
+}
+function purple() {
+    document.getElementById("color").innerHTML = "<h1>Purple</h1>";
+    document.getElementById("color").style.backgroundColor = "purple";
+    document.getElementById("image").src = "purple.png";
+    document.getElementById("color").style.color = "white";
+}
